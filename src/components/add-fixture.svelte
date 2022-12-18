74337aee10cd7496ec7571268577e54ce7a6c8a9
ralphie <ralpheiligan@gmail.com>
2022-12-18 17:58:01 -0500

    componentise addfixture

---
+++ src/components/add-fixture.svelte
@@ -0,0 +1,60 @@
+<script>
+	import { onMount } from 'svelte';
+	import toast, { Toaster } from 'svelte-french-toast';
+
+	export let fixtureName = '';
+	export let fixtureId = '';
+
+	const inputFixtureName = (e) => {
+		fixtureName = e.target.value;
+	};
+
+	const inputFixtureId = (e) => {
+		fixtureId = e.target.value;
+	};
+
+	// onMount(() => {
+	// 	toast.success('It works!');
+	// });
+
+	const handleClick = () => {
+		if (fixtureName === '' || fixtureId === '') {
+			toast.error('Incomplete information!');
+			return;
+		}
+		toast.success('New fixture added!');
+	};
+</script>
+
+<div class="flex justify-center">
+	<div class="card shadow-md bg-neutral-content mt-10 w-11/12 sm:w-4/12">
+		<div class="card-body">
+			<h2 class="card-title ">Add Fixture</h2>
+
+			<div class="form-control w-full max-w-xs">
+				<label class="label">
+					<span class="label-text">Fixture Id</span>
+				</label>
+				<input
+					type="text"
+					placeholder="fixture id"
+					class="input input-bordered w-full max-w-xs"
+					on:input={inputFixtureId}
+				/>
+				<label class="label">
+					<span class="label-text">Fixture name</span>
+				</label>
+				<input
+					type="text"
+					placeholder="fixture name"
+					class="input input-bordered w-full max-w-xs"
+					on:input={inputFixtureName}
+				/>
+			</div>
+			<div class="card-actions justify-start">
+				<button class="btn btn-primary" on:click={handleClick}>Add</button>
+			</div>
+		</div>
+	</div>
+	<Toaster />
+</div>
